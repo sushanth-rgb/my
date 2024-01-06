dfa93a0557819c3adde03acd3e120f5f3110983c
pandas342023 <shriyanprojects2023@gmail.com>
2024-01-06 16:52:21 +0530

    ok

--- src/day_twenthy/digit_extraction.java
+++ src/day_twenthy/digit_extraction.java
@@ -1,5 +1,18 @@
 package day_twenthy;
 
 public class digit_extraction {
-
+//digit extraction
+	public static void main(String[] args) {
+		int count=0;
+		int n=3345;
+		while(n>0) {
+			int last=n%10;
+			count++;
+			n=n/10;
+			System.out.println(last);
+			System.out.println(count);
+			System.out.println();
+		}
+	}
+	
 }
